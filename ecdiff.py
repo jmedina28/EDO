553d5af6a3e8ef4421fa9ea9943bfa14e62fb026
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-11 15:07:20 +0200

    ya tengo la primera

---
+++ ecdiff.py
@@ -0,0 +1,18 @@
+
+
+import matplotlib.pyplot as plt
+import sympy
+import numpy as np
+from scipy import integrate
+
+sympy.init_printing(use_latex='mathjax')
+
+x = sympy.Symbol('x')
+y = sympy.Function('y')
+
+f = (x**2 + 1)*(y(x)**-1 + 1)
+
+
+sol = sympy.dsolve(y(x).diff(x) - f, y(x))
+
+print(sol)
