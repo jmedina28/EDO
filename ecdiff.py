0ecb2abc624cca438edc907c98baa4ae28bedc9a
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-13 12:37:35 +0200

    resuelta la segunda ecuacion

--- ecdiff.py
+++ ecdiff.py
@@ -15,5 +15,13 @@ f = (x**2 + 1)*(y(x)**-1 + 1)
 
 sol = sympy.dsolve(y(x).diff(x) - f, y(x))
 
-print(sol)
 sympy.pprint(sol)
+
+x2 = sympy.symbols('x')
+y2 = sympy.Function('y')
+
+f2 = (y2(x2)*sympy.log(y2(x2), sympy.exp(1)))
+    
+sol2 = sympy.dsolve(y2(x2).diff(x2) - f2, y2(x2)) 
+print("*"*80)
+sympy.pprint(sol2)
