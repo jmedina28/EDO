a9706f5f1fc96fea2bb8fa35d653aadca2f99533
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-10-13 17:53:07 +0200

    acabado el ejercicio

--- lanzador.py
+++ lanzador.py
@@ -1,13 +1,14 @@
-from ecuaciones.ecdiff import ec1, ec2, ec3, ec4
+from ecuaciones.ecdiff import EDO
+
 seleccion = input("Seleccione la ecuación a resolver: ")
 def lanzar():
     if seleccion == "1":
-        ec1()
+        EDO.ec1()
     elif seleccion == "2":
-        ec2()
+        EDO.ec2()
     elif seleccion == "3":
-        ec3()
+        EDO.ec3()
     elif seleccion == "4":
-        ec4()
+        EDO.ec4()
     else:
         print("Opción no válida")
